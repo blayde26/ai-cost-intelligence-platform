31e13b5a9e5a98ae79ecf5fc71a3065b1487cbf2
Brian Layden <84464314+blayde26@users.noreply.github.com>
2026-05-30 09:04:30 -0400

    Wire Sprint 2 Jira configuration

--- src/main/java/com/acip/AcipApplication.java
+++ src/main/java/com/acip/AcipApplication.java
@@ -1,12 +1,13 @@
 package com.acip;
 
 import com.acip.proxy.OpenAiProperties;
+import com.acip.jira.JiraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(OpenAiProperties.class)
+@EnableConfigurationProperties({OpenAiProperties.class, JiraProperties.class})
 public class AcipApplication {
 
     public static void main(String[] args) {
