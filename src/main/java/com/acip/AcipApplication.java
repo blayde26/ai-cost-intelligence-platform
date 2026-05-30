3c72c49b01feb6cf09084b09f8696061c117a920
Brian Layden <84464314+blayde26@users.noreply.github.com>
2026-05-30 08:51:57 -0400

    Add Spring Boot application

---
+++ src/main/java/com/acip/AcipApplication.java
@@ -0,0 +1,15 @@
+package com.acip;
+
+import com.acip.proxy.OpenAiProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(OpenAiProperties.class)
+public class AcipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcipApplication.class, args);
+    }
+}
